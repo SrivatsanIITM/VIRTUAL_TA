e450fe39f5d447c4f1d4395cb257c4752e864312
SrivatsanIITM <23f2000296@ds.study.iitm.ac.in>
2025-06-18 20:53:44 +0530

    port

--- main.py
+++ main.py
@@ -195,3 +195,8 @@ async def ask_query(data: QueryInput):
 
 print("Visit http://localhost:8000/docs in your browser")
 # uvicorn.run(app, host="127.0.0.1", port=8000)
+if __name__ == "__main__":
+    import uvicorn
+    # 👇 Use the port assigned by Render
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
